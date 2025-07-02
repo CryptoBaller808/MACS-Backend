b8e90e98f50a768e8185c7ab7294f75d4b3b2eb5
CryptoBaller808 <82867782+CryptoBaller808@users.noreply.github.com>
2025-07-02 09:36:27 +0700

    Update errorHandler.js

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,5 @@
-// src/middleware/errorHandler.js
 module.exports = (err, req, res, next) => {
-  console.error('🔥 ErrorHandler:', err.stack);
+  console.error('🔥 ErrorHandler:', err.stack || err);
   res.status(err.status || 500).json({
     error: err.message || 'Internal Server Error',
   });
